6be8e7c6f1d1e9ab6e212de020471e2a03db86d7
Ангелина <coffeefriend12r@gmail.com>
2024-08-11 10:51:10 +0300

    11

--- .nuxt/prerender/chunks/build/styles.mjs
+++
@@ -1,9 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.CQLG8hYm.mjs').then(interopDefault),
-  "error.vue": () => import('./error-styles.D71NfQXh.mjs').then(interopDefault),
-  "error.vue?vue&type=style&index=0&scoped=1de0b1b2&lang.css": () => import('./error-styles.C-HkfLbC.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
